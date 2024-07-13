e91e236eea224626544bb8d1a9d0ae1f044e1137
Kadek Widiana <kdkonos10@gmail.com>
2024-07-13 15:51:18 +0800

    dump autload

--- app/Models/OutreachActivities.php
+++ app/Models/OutreachActivities.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class OutreachActivity extends Model
+class OutreachActivities extends Model
 {
     use HasFactory;
 
