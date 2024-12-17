cfd6c30ef5a2490e59d8fc66c8dce77d2c0b3041
Kadek Widiana <kdkonos10@gmail.com>
2024-12-17 08:07:30 +0800

    add root loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,52 @@
       <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     @endif
 
+    <style>
+      .loading {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.9);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 9999;
+      }
+
+      .preloader {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+      }
+
+      .preloader-logo {
+        width: 150px;
+        margin-bottom: 5px;
+      }
+
+      .preloader-text {
+        font-weight: bold;
+        font-family: monospace;
+        color: #3C3D37;
+        font-size: 25px;
+        clip-path: inset(0 3ch 0 0);
+        animation: l4 1s steps(4) infinite;
+      }
+
+      .preloader-text:before {
+        content: "Loading...";
+      }
+
+      @keyframes l4 {
+        to {
+          clip-path: inset(0 -1ch 0 0);
+        }
+      }
+    </style>
+
     <!-- Scripts -->
     @routes
     @viteReactRefresh
@@ -25,7 +71,21 @@
   </head>
 
   <body class="font-sans antialiased bg-slate-100">
+    <div id="loading-root" class="loading">
+      <div class="preloader">
+        <img src="/assets/images/logo-distan-buleleng-2.png" alt="Logo Distan Buleleng" class="preloader-logo" />
+        <span class="preloader-text"></span>
+      </div>
+    </div>
     @inertia
   </body>
+  <script>
+    const preloader = document.querySelector('#loading-root');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove();
+      });
+    }
+  </script>
 
 </html>
